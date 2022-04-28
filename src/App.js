b87c6972ce79dc176443947a0451e1e02145636f
Michael Stafford <michaelstafford808@gmail.com>
2022-04-28 16:45:14 -0700

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div>    
-    <h1>Hello World </h1>   
+    <h1>Hello World </h1>
+    <Button>Home</Button>
     </div>
   );
 }
