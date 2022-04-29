4ba21aa59800a36f66ce5059baac2ca3b158317d
Michael Stafford <michaelstafford808@gmail.com>
2022-04-28 17:25:21 -0700

    imported BrowserRouter, Routes, and Route into App.js and created Basic Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import Button from '@mui/material/Button';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/static/authentication/Login';
+import Signup from './components/static/authentication/Signup';
+import HomePage from './components/static/HomePage';
+import Navbar from './components/static/navigation/Navbar';
 
 function App() {
   return (
-    <div>    
-    <h1>Hello World </h1>
-    <Button>Home</Button>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={ <HomePage/> } />
+      <Route path="/login" element={ <Login/> } />
+      <Route path="/signup" element={ <Signup/> } />
+    </Routes>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/components/static/navigation/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+
+function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Crypto Gecko
+        </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">Login</Button>
+        <Button color="inherit">Signup</Button>
+      </Toolbar>
+    </AppBar>
+  </Box>
+  )
+}
+
+export default Navbar;
